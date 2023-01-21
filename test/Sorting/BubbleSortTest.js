05d27a2aeb1249db52637fd34a678263ed43dc7c
Renato Moura Martins Medeiros <renamedrj@gmail.com>
2023-01-21 01:28:36 -0300

    Complete bubblesort testing

--- test/Sorting/BubbleSortTest.js
+++ test/Sorting/BubbleSortTest.js
@@ -52,3 +52,37 @@ import SortingOrder from "../../src/Sorting/SortingOrder.js";
       });
     });
   });
+
+  [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+  .forEach((elemCount) => {
+    describe(`BubbleSort`, () => {
+      it(`Should Sort Ascending When array has ${elemCount} complex element(s)`, () => {
+        // Arrange
+        const numbers = Array.from({length: elemCount}, u => ({age: 1 + Math.trunc(Math.random() * 100)}))
+
+        // Act
+        const result = bubbleSort(numbers, SortingOrder.ASCENDING, (a, b) => a.age - b.age);
+
+        // Assert
+        expect(result).lengthOf(elemCount);
+        expect(result.every((x, i) => i === 0 || x >= result[i - 1])).to.be.true;
+      });
+    });
+  });
+
+  [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+  .forEach((elemCount) => {
+    describe(`BubbleSort`, () => {
+      it(`Should Sort Descending When array has ${elemCount} complex element(s)`, () => {
+        // Arrange
+        const numbers = Array.from({length: elemCount}, u => ({age: 1 + Math.trunc(Math.random() * 100)}))
+
+        // Act
+        const result = bubbleSort(numbers, SortingOrder.DESCENDING, (a, b) => a.age - b.age);
+
+        // Assert
+        expect(result).lengthOf(elemCount);
+        expect(result.every((x, i) => i === 0 || x <= result[i - 1])).to.be.true;
+      });
+    });
+  });
